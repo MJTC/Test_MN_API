5c169df78fbfc4506bfa0c4b890ee86c418b5711
Marco Jansen <mjjjansen@gmail.com>
2022-06-16 13:29:48 +0200

    add routes

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const jsonServer = require('json-server');
-const routes = require('routes.json');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
-server.use('/', routes);
 server.use(middlewares);
 server.use(router);
 
